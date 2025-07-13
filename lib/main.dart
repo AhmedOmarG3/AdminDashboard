1e36488c4ba75d5bba10fbb275b14b0e7b2235f9
Ahmed Omar <omar155432@gmai.com>
2025-07-13 18:28:49 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AdminDashboard());
+}
+
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    );
+  }
+}
+
+
